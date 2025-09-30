d2060421cb966120f12f158e6faede8173ba9d79
Hana Snow <hsnow@broadinstitute.org>
2025-09-30 17:35:11 -0400

    first pass consequence filter

--- ui/pages/SummaryData/components/GeneVariantLookup.jsx
+++ ui/pages/SummaryData/components/GeneVariantLookup.jsx
@@ -4,10 +4,38 @@ import { Grid } from 'semantic-ui-react'
 
 import { validators } from 'shared/components/form/FormHelpers'
 import FormWrapper from 'shared/components/form/FormWrapper'
+import { AlignedCheckboxGroup } from 'shared/components/form/Inputs'
 import SubmitFormPage from 'shared/components/page/SubmitFormPage'
-import { GENOME_VERSION_FIELD } from 'shared/utils/constants'
+import {
+  GENOME_VERSION_FIELD,
+  GROUPED_VEP_CONSEQUENCES,
+  VEP_GROUP_NONSENSE,
+  VEP_GROUP_ESSENTIAL_SPLICE_SITE,
+  VEP_GROUP_FRAMESHIFT,
+  VEP_GROUP_SYNONYMOUS,
+  VEP_GROUP_EXTENDED_SPLICE_SITE,
+  VEP_GROUP_OTHER,
+} from 'shared/utils/constants'
+import { snakecaseToTitlecase } from 'shared/utils/stringUtils'
+
+const CONSEQUENCE_FILEDS = [
+  VEP_GROUP_NONSENSE,
+  VEP_GROUP_ESSENTIAL_SPLICE_SITE,
+  VEP_GROUP_FRAMESHIFT,
+  VEP_GROUP_SYNONYMOUS,
+  VEP_GROUP_EXTENDED_SPLICE_SITE,
+  VEP_GROUP_OTHER,
+].map(group => ({
+  name: `annotations.${group}`,
+  component: AlignedCheckboxGroup,
+  groupLabel: snakecaseToTitlecase(group),
+  options: GROUPED_VEP_CONSEQUENCES[group],
+  format: value => value || [],
+  maxOptionsPerColumn: 7,
+}))
 
 const FIELDS = [
+  ...CONSEQUENCE_FILEDS,
   { validate: validators.required, ...GENOME_VERSION_FIELD },
 ]
 
